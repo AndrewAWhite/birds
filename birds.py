6da836fb18360df201ff0f4500520ce6ca0229ac
Andrew <andrew.white@ampereanalysis.com>
2022-11-15 19:17:43 +0000

    experimenting with different acceleration

--- birds.py
+++ birds.py
@@ -7,6 +7,17 @@ import math
 MENU_W = 300
 SCREEN_W = 1800	
 SCREEN_H = 980
+WINDOW_W = SCREEN_W - MENU_W
+
+BIRDS=None
+BIRD_COUNT=500
+
+
+ACCELERATION = 1
+REPULSIVE_DISTANCE = 2
+REPULSIVE_POWER = 2
+MAX_SPEED = 10000
+MAX_DISTANCE = 200
 
 def get_random_direction():
 	return -1^(random.randint(1, 2))
@@ -15,87 +26,126 @@ def find_closest_birds(b, birds):
 	return sorted(birds, key=lambda cb: math.sqrt((math.pow((cb.x-b.x), 2) + math.pow((cb.y - b.y), 2))))[:2]
 
 class Bird(pygame.sprite.Sprite):
-	def __init__(self, i, space):
+	def __init__(self, i, grid):
 		super().__init__()
 		self.i = i
-		self.x = random.randint(MENU_W, SCREEN_W-MENU_W-10)
+		self.grid = grid
+		self.x = random.randint(MENU_W, WINDOW_W-10)
 		self.y = random.randint(1, SCREEN_H-10)
-		self.space = space
-		while self.space[self.x][self.y] is not None:
-			self.x = random.randint(MENU_W, SCREEN_W-MENU_W-10)
-			self.y = random.randint(1, SCREEN_H-10)
-		self.space[self.x][self.y] = self
 		self.v = [0, 0]
+		self.grid_key = self.get_grid_key()
+		self.update_grid()
 		self.surf = pygame.Surface((3, 3))
 		self.surf.fill((255,255,255))
 		self.rect = self.surf.get_rect(center = (3, 3))
+		self.neigbours = None
+		self.get_neighbours()
+
+	def get_grid_key(self):
+		x = math.floor(self.x / WINDOW_W)*10
+		y = math.floor(self.y/SCREEN_W)*10
+		return (x,y)
+
+	def update_grid(self):
+		cur_spot = self.grid.get(self.grid_key, set())
+		if self in cur_spot:
+			cur_spot.remove(self)
+		self.grid[self.grid_key] = cur_spot
+		neighbours = self.grid.get(self.get_grid_key(), set())
+		neighbours.add(self)
+		self.grid[self.get_grid_key()] = neighbours
+		self.grid_key = self.get_grid_key()
+
+	def get_distance(self, x, y):
+		dx = x - self.x
+		dy = y - self.y
+		d = math.sqrt(math.pow(dx, 2) + math.pow(dy, 2))
+		return d
 
 	def get_neighbours(self):
-		closest_birds = []
-		for cb in self.space[self.x][self.y:]:
-			if cb is not None and cb.i != self.i:
-				closest_birds.append(cb)
-				break
-		for cb in reversed(self.space[self.x][:self.y]):
-			if cb is not None and cb.i != self.i:
-				closest_birds.append(cb)
-				break
-		ob = False
-		for sr in reversed(self.space[:self.x]):
-			for cb in sr[self.y:]:
-				if cb is not None and cb.i != self.i:
-					closest_birds.append(cb)
-					ob = True
-					break
-			for cb in reversed(sr[:self.y]):
-				if cb is not None and cb.i != self.i:
-					closest_birds.append(cb)
-					ob = True
-					break
-			if ob:
-				break
-		ob = False
-		for sr in self.space[self.x:]:
-			for cb in sr[self.y:]:
-				if cb is not None and cb.i != self.i:
-					closest_birds.append(cb)
-					ob = True
-					break
-			for cb in reversed(sr[:self.y]):
-				if cb is not None and cb.i != self.i:
-					closest_birds.append(cb)
-					ob = True
-					break
-			if ob:
-				break
-		return closest_birds
+		if self.neigbours is None:
+			neighbours =  self.grid.get(self.grid_key, set())
+			if len(neighbours) == 0:
+				pass
+			self.neigbours = [cb for cb in neighbours if cb.i != self.i and self.get_distance(cb.x, cb.y) < MAX_DISTANCE][:10]
+		for cb in self.neigbours:
+			if self.get_distance(cb.x, cb.y) < MAX_DISTANCE:
+				continue
+			self.neigbours.remove(cb)
+			neighbours =  self.grid.get(self.grid_key, set())
+			candidates = sorted(self.neigbours, key=lambda cb: self.get_distance(cb.x, cb.y))
+			if candidates:
+				self.neigbours.append(candidates[0])
+			
+	def get_attraction(self, x, y):
+		ax =  ACCELERATION * math.pow(x - self.x, 2)
+		ay = ACCELERATION * math.pow(y - self.y, 2)
+		if x - self.x < 0:
+			ax *= -1
+		if y - self.y < 0:
+			ay *= -1
+		return ax, ay
 
-	def get_accel_fn(self, d):
-		return  ((1 / (1 + math.pow(2, -(abs(d)-2)))) - 0.5)
+	def get_repulsion(self, x, y):
+		try:
+			ax = ACCELERATION / (1-math.pow(2, -abs(self.x-x)))
+		except:
+			ax = 100
+		try:
+			ay = ACCELERATION / (1-math.pow(2, -abs(self.y-y)))
+		except:
+			ay = 100
+		if x - self.x < 0:
+			ax *= -1
+		if y - self.y < 0:
+			ay *= -1
+		return ax, ay
 
-	def determine_attraction(self, cb):
-		dx = cb.x - self.x
-		dy = cb.y - self.y
-		accel_x = self.get_accel_fn(dx)
-		accel_y = self.get_accel_fn(dy)
-		if dx < 0:
-			accel_x *= -1
-		if dy < 0:
-			accel_y *= -1
-		return accel_x, accel_y
+	def determine_acceleration(self, x, y):
+		ax, ay = self.get_attraction(x, y)
+		return ax, ay
 
 	def accelerate(self):
-		closest_birds = self.get_neighbours()
-		for cb in closest_birds:
-			accel_x, accel_y = self.determine_attraction(cb)
+		self.get_neighbours()
+		for cb in self.neigbours:
+			accel_x, accel_y = self.determine_acceleration(cb.x, cb.y)
 			self.v[0] += accel_x
-			self.v[1] += accel_y
+			self.v[1] += accel_y 
+		speed = math.sqrt(math.pow(self.v[0], 2) + math.pow(self.v[1], 2))
+		mx, my = pygame.mouse.get_pos()
+		amx, amy = self.get_repulsion(mx, my)
+		self.v[0] -= 10*amx
+		self.v[1] -= 10*amy
+		if speed > (MAX_SPEED/100):
+			if self.v[0] == 0:
+				t = math.pi/2
+			else:
+				t = math.atan(self.v[1]/self.v[0])
+			self.v[0] = (MAX_SPEED/1000) * math.cos(t)
+			self.v[1] = (MAX_SPEED/1000) * math.sin(t)
+		elif speed < 1:
+			self.v[0] = 100*random.random()
+			self.v[1] = 100*random.random()
 
 	def move(self):
-		self.accelerate()
 		self.x += int(self.v[0])
 		self.y += int(self.v[1])
-
+		if self.x > SCREEN_W and self.v[0] > 0:
+			self.v[0] *= 0.9
+			self.x = MENU_W
+		if self.x < MENU_W and self.v[0] < 0:
+			self.v[0] *= 0.9
+			self.x = SCREEN_W
+		if self.y > SCREEN_H and self.v[1] > 0:
+			self.v[1] *= 0.9
+			self.y = 0
+		if self.y < 0 and self.v[1] < 0:
+			self.v[1] *= 0.9
+			self.y = SCREEN_H
+		self.update_grid()
+		self.accelerate()
+		
+		
 
 def get_theme():
 	theme = pygame_menu.Theme(
@@ -103,7 +153,7 @@ def get_theme():
 		widget_font=pygame_menu.font.FONT_NEVIS,
 		widget_alignment=pygame_menu.locals.ALIGN_LEFT,
 		title_font_size=20,
-		widget_font_size=20,
+		widget_font_size=10,
 		border_color=((255, 255, 255, 255)),
 		border_width=1,
 		widget_border_width=0,
@@ -112,18 +162,40 @@ def get_theme():
 	)
 	return theme
 
+def reset_birds():
+	global BIRDS
+	grid = {}
+	BIRDS = [Bird(i, grid) for i in range(int(BIRD_COUNT))]
+
+
 def get_menu():
 	theme = get_theme()
 	menu = pygame_menu.Menu('Birds', MENU_W, SCREEN_H, theme=theme, position=(0, 0))
-	menu.add.range_slider('stickiness', 10, [0,100], 1, rangeslider_id='stickiness', border_width=0, border_position=pygame_menu.locals.POSITION_NORTH)
-	menu.add.range_slider('trickiness', 10, [0,100], 1, rangeslider_id='trickiness', border_width=0, border_position=pygame_menu.locals.POSITION_NORTH)
+	menu.add.range_slider('bird count', 500, [20,1000], 1, rangeslider_id='bird_count', border_width=0, border_position=pygame_menu.locals.POSITION_NORTH)
+	menu.add.range_slider('Acceleration', 5, [0,100], 1, rangeslider_id='acceleration', border_width=0, border_position=pygame_menu.locals.POSITION_NORTH)
+	menu.add.range_slider('repulsive distance', 10, [0,100], 1, rangeslider_id='repulsive_distance', border_width=0, border_position=pygame_menu.locals.POSITION_NORTH)
+	menu.add.range_slider('repulsive power', 10, [0,100], 1, rangeslider_id='repulsive_power', border_width=0, border_position=pygame_menu.locals.POSITION_NORTH)
+	menu.add.range_slider('max speed', 10000, [0,10000], 1, rangeslider_id='max_speed', border_width=0, border_position=pygame_menu.locals.POSITION_NORTH)
+	menu.add.range_slider('max distance', 250, [50, 300], 1, rangeslider_id='max_distance', border_width=0, border_position=pygame_menu.locals.POSITION_NORTH)
+	menu.add.button('reset', reset_birds)
 	return menu
 
 def scale_slider_pos(slider_val):
 	return int(MENU_W + slider_val * (SCREEN_W-MENU_W)/100)
 
+
+def set_widget_vals(menu):
+	global ACCELERATION, REPULSIVE_DISTANCE, MAX_SPEED, REPULSIVE_POWER, BIRD_COUNT, MAX_DISTANCE
+	ACCELERATION = menu.get_widget('acceleration').get_value()
+	REPULSIVE_DISTANCE = menu.get_widget('repulsive_distance').get_value()
+	REPULSIVE_POWER = menu.get_widget('repulsive_power').get_value()
+	MAX_SPEED = menu.get_widget('max_speed').get_value()
+	BIRD_COUNT = menu.get_widget('bird_count').get_value()
+	MAX_DISTANCE = menu.get_widget('max_distance').get_value()
+
 # define a main function
 def main():
+	
 	FramePerSec = pygame.time.Clock()
 	# initialize the pygame module
 	pygame.init()
@@ -132,14 +204,14 @@ def main():
 	menu = get_menu()
 	# define a variable to control the main loop
 	running = True
-	space = [[None for _ in range(SCREEN_H)] for _ in range(SCREEN_W-MENU_W)]
-	birds = [Bird(i, space) for i in range(2)]
+	reset_birds()
 	# main loop
 	while running:
 		screen.fill((0,0,0))
-		for b in birds:
+		for b in BIRDS:
 			b.move()
 			screen.blit(b.surf, (b.x, b.y))
+		set_widget_vals(menu)
 		#pygame.display.flip()
 		# event handling, gets all event from the event queue
 		events = pygame.event.get()
